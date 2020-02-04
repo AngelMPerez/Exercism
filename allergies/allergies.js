295e032041a0ff0128c0878002765623028025a2
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-03 18:39:21 -0700

    in progress

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -5,22 +5,41 @@
 
 export class Allergies {
   constructor(numb) {
-    this.allergie=[]
+    this.allergie={
+      arr:[],
+      index:[]
+    }
     for(let i=7;i>=0;i--){
       // console.log(Math.pow(2,i))
       if(numb-Math.pow(2,i)>=0){
-        this.allergie.push(i)
-        numb-=2^i
+        this.allergie.index.push(i)
+        numb-=Math.pow(2,i)
       }
     }
+    console.log(this.allergie.index)
   }
 
   list() {
-    let array = []
-    this.allergie
+    let array = ['eggs','peanuts','shellfish','strawberries','tomatoes','chocolate','pollen','cats']
+    for(let i=0,j=0;i<this.allergie.index.length;i++){
+      if(j==this.allergie.index[i]){
+        this.allergie.arr.push(array[i])
+        j=-1
+      }
+      j++
+    }
+    console.log(this.allergie.arr)
+    return this.allergie.arr
   }
 
-  allergicTo() {
-    
+  allergicTo(string) {
+    this.list()
+    console.log(this.allergie.arr)
+    for(let i=0;i<this.allergie.arr.length;i++){
+      if(string==this.allergie.arr[i]){
+        return true
+      }
+    }
+    return false
   }
 }
