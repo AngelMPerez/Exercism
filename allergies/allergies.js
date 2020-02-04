1cc19f5dee89b21004bf4d62d2aa505f0b3c900a
Angel Perez <angelmperezcarranza@gmail.com>
2020-02-04 12:29:31 -0700

    finish

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -5,31 +5,29 @@
 
 export class Allergies {
   constructor(numb) {
-    this.allergie=[]
-    while(numb>256){numb=numb-256}
+    this.allergie=[]//array with the number of the allergies
+    while(numb>256){
+      numb=numb-256
+    }
     for(let i=7;i>=0;i--){
-      // console.log(Math.pow(2,i))
       if(numb-Math.pow(2,i)>=0){
         this.allergie.push(i)
         numb-=Math.pow(2,i)
       }
     }
-    // console.log(this.allergie)
   }
 
   list() {
     let array = ['eggs','peanuts','shellfish','strawberries','tomatoes','chocolate','pollen','cats']
-    let array2 = []
+    let array2 = []//list with the allergies of the person
     for(let i=0;i<this.allergie.length;i++){
       array2.unshift(array[this.allergie[i]])
     }
-    // console.log(array2)
     return array2
   }
 
   allergicTo(string) {
-    let arr=this.list()
-    // console.log(arr)
+    let arr=this.list()//this needs the array of allergies to see if the person is allergic to something
     for(let i=0;i<arr.length;i++){
       if(string==arr[i]){
         return true
